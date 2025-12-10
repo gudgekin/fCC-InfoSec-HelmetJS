387a8bed5e12498929271bfe21a61681c75d7cd9
gudgekin <86387331+gudgekin@users.noreply.github.com>
2025-12-10 11:14:36 -0800

    lesson 3 clickjacking

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const app = express();
 const helmet = require('helmet');
 // app.use(helmet);
 app.use(helmet.hidePoweredBy({ setTo: 'hello'}));
-
+app.use(helmet.frameguard({ action: 'deny' }));
 
 
 
