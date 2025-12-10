a6085f7ef759fa386602ef83e0ce3c565dddd556
gudgekin <86387331+gudgekin@users.noreply.github.com>
2025-12-10 12:53:14 -0800

    lesson 7 use only https

--- myApp.js
+++ myApp.js
@@ -8,7 +8,8 @@ app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-
+let timeInSeconds = 7776000;
+app.use(helmet.hsts({ maxAge: timeInSeconds, force: true }));
 
 
 
