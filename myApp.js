24045b8cec9be1e3f7f5c6223453eab21ab085d8
gudgekin <86387331+gudgekin@users.noreply.github.com>
2025-12-10 13:07:30 -0800

    lesson 9 disable client-side caching

--- myApp.js
+++ myApp.js
@@ -11,6 +11,7 @@ app.use(helmet.ieNoOpen());
 let timeInSeconds = 7776000;
 app.use(helmet.hsts({ maxAge: timeInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 
 
